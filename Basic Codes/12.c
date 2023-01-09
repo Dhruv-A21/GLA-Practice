bc0a300df35a13282598b96514fbcd4cff5d036c
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-01-09 20:54:03 +0530

    Create 12.c

---
+++ Basic Codes/12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+    //Finding area of Trianglr
+    float x,y,z;
+    printf("Enter the value of Base x=");
+    scanf("%f",&x);
+    printf("Enter the value of Height y=");
+    scanf("%f",&y);
+    
+    z=x*y/2;
+    printf("\nSo Area of Triangle= %f",z);
+    
+    
+
+    return 0;
+}
