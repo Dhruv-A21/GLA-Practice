87e9eee74aeba9fcdbbe469a371450a0bedc965f
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-01-09 20:50:11 +0530

    Create 1.c

---
+++ Basic Codes/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    //To find the sum of 2 numbers
+    int a,b,c;
+    printf("\nEnter the First number=");
+    scanf("%d",&a);
+    printf("\nEnter the Second number=");
+    scanf("%d",&b);
+    c=a+b;
+
+    printf("\nSum of two numbers= %d",c);
+
+    return 0;
+}
