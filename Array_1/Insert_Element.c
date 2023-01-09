57b7b0923cc8345cc9ab2fb1f60a4a75bc85a437
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-01-09 21:11:45 +0530

    Create Insert_Element.c

---
+++ Array_1/Insert_Element.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+//Insert an element at position X;
+int main(){
+    int N;
+    printf("Enter the Array Size:");
+    scanf("%d",&N);
+    int a[N+1];
+
+    printf("Enter the Array:");
+    for(int i=0;i<N;i++){
+        scanf("%d",&a[i]);
+    }
+    int ele,pos;
+    printf("Enter the element and position:");
+    scanf("%d %d",&ele,&pos);
+    for(int i=pos;i<N+1;i++){
+        int temp=a[i];
+        a[i]=ele;
+        ele=temp;
+    }
+    printf("New Array:");
+    for(int i=0;i<N+1;i++){
+        printf("%d ",a[i]);
+
+    }
+    return 0;
+}
