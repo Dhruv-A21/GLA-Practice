c4def3742da6692270be6cc781463023123c826f
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-01-09 20:53:33 +0530

    Create 10.c

---
+++ Basic Codes/10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    //Sqrt of x
+    float x,z;
+    printf("Enter the value of base x=");
+    scanf("%f",&x);
+    
+    z=sqrt(x);
+    printf("\nSo Sqrt of x= %f",z);
+    
+    
+
+    return 0;
+}
