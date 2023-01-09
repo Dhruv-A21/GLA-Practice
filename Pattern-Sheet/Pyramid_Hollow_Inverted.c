68371f45f74cb46ac3990e242a8c941ff04e0c4b
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-01-09 21:02:32 +0530

    Create Pyramid_Hollow_Inverted.c

---
+++ Pattern-Sheet/Pyramid_Hollow_Inverted.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    for(int i=5;i>=1;i--){
+        for(int j=1;j<=5-i;j++){
+            printf("  ");
+        }
+        for(int k=1;k<=(2*i)-1;k++){
+            if(i==5||k==1||k==(2*i)-1){
+            printf("* ");
+            continue;
+            }
+            printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
