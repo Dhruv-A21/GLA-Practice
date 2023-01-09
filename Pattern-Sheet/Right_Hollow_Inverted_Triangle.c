f7f0dd374333f764c1ddf9aeefbbe21bb24e15ff
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-01-09 21:04:58 +0530

    Create Right_Hollow_Inverted_Triangle.c

---
+++ Pattern-Sheet/Right_Hollow_Inverted_Triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=6-i;j++){
+            if(i==1||j==1||j==6-i){
+            printf("* ");
+            continue;
+            }
+            printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
